660740f14548341c6573665229ded065da7b0544
LowkeyApollo <aleman.josue@icloud.com>
2022-12-29 17:29:53 -0600

    still setting up

--- Main.py
+++ Main.py
@@ -1,2 +1,31 @@
-print('Hello World')
-#hi
+# This example requires the 'message_content' intent.
+
+import discord
+import os
+import asyncio
+from dotenv import load_dotenv, find_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+load_dotenv(find_dotenv())
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+async def main():
+    async with client:
+        await load()
+        await client.start(os.getenv('DISCORD_TOKEN'))
+
+asyncio.run(main())
